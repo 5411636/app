c1d215c2a881d963cfdc3812dbc65d5a96b78b61
jjh <1130252319@qq.com>
2025-12-31 16:23:15 +0800

    第二周第8套

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,19 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import  List  from "../views/list"
-import Add from "../views/add"
-import Edit from "../views/edit"
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <List />,
-    },
-    {
-        path: "/add",
-        element: <Add />,
-    },
-    {
-        path: "/edit",
-        element: <Edit />,
     }
 ])
 export default router;
--- src/views/list.jsx
+++ src/views/list.jsx
@@ -1,107 +1,141 @@
 import React, { Component } from 'react'
-import { NavBar } from 'react-vant';
 import axios from 'axios'
-import { Cell, Image } from 'react-vant';
-import { Radio } from 'react-vant';
-import { ActionBar } from 'react-vant';
-//跳转 路由
-import { Link } from 'react-router-dom'
-const navigator = (url) => {
-    window.location.href = url
-}
+import { Input, Button } from 'antd'
+import { Radio } from 'antd';
+
 export class list extends Component {
     componentDidMount() {
         this.getlist()
     }
+
     state = {
         list: [],
-        guan: '管理',
-        id: 0
+        fang: '',
+        jilu: JSON.parse(localStorage.getItem('jilu')) || [],
+        ji: '不限',
+        options: [
+            { label: '不限', value: '不限' },
+            { label: '北京', value: '北京' },
+            { label: '上海', value: '上海' },
+            { label: '广州', value: '广州' },
+            { label: '深圳', value: '深圳' },
+        ],
+        sor:'asc'
+
     }
+
+
     async getlist() {
-        const res = await axios.get('http://localhost:3000/list')
+        const params = {
+            _sort: 'price',
+            _order: this.state.sor
+        }
+        if (this.state.fang != '') {
+            params.name_like = this.state.fang
+        }
+        if (this.state.ji != '不限') {
+            params.address = this.state.ji
+        }
+        const res = await axios.get('http://localhost:3000/list', { params })
         this.setState({
             list: res.data
         })
+        
     }
-    async dian(item) {
-        this.state.list.map(item1 => {
-            if (item1.id !== item.id) {
-                item1.state = false
-            }
-            if (item1.id === item.id) {
-                item1.state = true
-            }
-            const res = axios.put(`http://localhost:3000/list/${item1.id}`, item1)
-            return res
-        }
-
-        )
-        this.getlist()
-
-
-
-    }
-    di(item) {
+    sou = (e) => {
         this.setState({
-            id: item.id
+            fang: e.target.value
         })
-
     }
-    tiao(){
-        //跳转
-        navigator('/add')
-    }
-    ed(item){
-        localStorage.setItem('item',JSON.stringify(item))
-        navigator('/edit')
-    }
-    async del(id){
-        if(! window.confirm('确定删除吗'))return
-        const res = await axios.delete(`http://localhost:3000/list/${id}`)
+    shua = () => {
         this.getlist()
+    }
+    s = () => {
+        //搜索记录 本地存储 前面添加
+        let jilu = this.state.jilu
+        jilu.unshift(this.state.fang)
+        this.setState({
+            jilu
+        })
+        localStorage.setItem('jilu', JSON.stringify(jilu))
+        this.getlist()
+    }
+    clear = () => {
+        this.setState({
+            jilu: [],
+            fang: ''
+        })
+        localStorage.removeItem('jilu')
+        this.getlist()
+    }
+    so = () => {
+        this.setState({
+            sor: this.state.sor == 'asc' ? 'desc' : 'asc'
+        }, () => {
+            this.getlist()            
+        })
+    }
+    c = () => {
+        this.setState({
+            fang: '',
+            ji: '不限'
+        }, () => {
+            this.getlist()
+        })
+        localStorage.removeItem('jilu')
+        this.setState({
+            jilu: []
+        })
     }
     render() {
         return (
             <div>
-                <NavBar
-                    title="我的收货地址"
-                    leftText="返回"
-                    rightText={this.state.guan}
-                />
-                {
-                    this.state.list.map(item => {
-                        return (
-
-                            <div key={item.id}>
-                                <Cell
-                                    center
-                                    key={item.id}
-                                    title={`${item.name} ${item.phone} ${item.state ? '默认' : ''}`}
-                                    label={`${item.address} ${item.xaddress}`}
-                                    icon={<Image width={44} height={44} src={item.img} round />}
-                                    isLink
-                                    onClick={() => this.di(item)}
-                                />
-                                <p style={{ display: this.state.id == item.id ? 'inline' : 'none' }}>
-                                    <Radio checked={item.state} onClick={() => this.dian(item)} >默认地址</Radio>
-                                    <span onClick={()=>this.del(item.id)}>删除</span><span onClick={()=>this.ed(item)}>编辑</span>
-                                </p>
-                            </div>
-
-
-                        )
-                    })
+                <Input value={this.state.fang} onChange={(e) => this.sou(e)} style={{ width: '200px' }} placeholder="请输入楼房名称" />
+                <Button type="primary" onClick={() => this.s()}>搜索</Button><br></br>
+                {this.state.jilu.map((item, index) => {
+                    return item
+                })
                 }
-                <div className='demo-action-bar'>
-                    <ActionBar>
-                        <ActionBar.Button
-                            type='danger'
-                            text='立即添加'
-                            onClick={()=>this.tiao()}
-                        />
-                    </ActionBar>
+                <Button type="primary" onClick={() => this.clear()}>清空搜索历史</Button>
+                <Radio.Group
+                    block
+                    options={this.state.options}
+                    defaultValue={this.state.ji}
+                    value={this.state.ji}
+                    optionType="button"
+                    buttonStyle="solid"
+                    style={{
+                        width: '400px',
+                    }}
+                    onChange={(e) => {
+                        this.setState({
+                            ji: e.target.value
+                        }, () => {
+                            console.log(this.state.ji);
+                            this.getlist()
+                        })
+                    }}
+                /><br></br>
+                <Button type="primary" onClick={() => this.so()}>价格排序</Button>
+                <p onClick={()=> this.c()}>清空条件</p>
+
+                <div>
+                    {
+                        this.state.list.map((item, index) => {
+                            return <div key={item.id} style={{ display: 'flex' }}>
+                                <img style={{ width: '100px', height: '100px' }} src={item.img} alt="" />
+                                <div style={{ marginLeft: '20px' }}>
+                                    <h3>{item.name} </h3>
+                                    <p>{item.hu} {item.address}</p>
+                                </div>
+                                <div style={{ marginLeft: '20px' }}>
+                                    <p style={{ color: 'red', fontSize: '20px' }}>{item.price}/元</p>
+                                </div>
+                            </div>
+                        })
+                    }
                 </div>
+
             </div>
         )
     }
