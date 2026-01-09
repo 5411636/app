b70a64a401dcef198a9bb4375badb99f36a16903
jjh <1130252319@qq.com>
2026-01-09 10:33:13 +0800

    git分支测试

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,14 +1,10 @@
 import { createBrowserRouter, redirect } from "react-router-dom";
 import  List  from "../views/list"
-import Searc from "../views/search"
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <List />,
-    },
-    {
-        path: "/search",
-        element: <Searc />,
     }
 ])
 
